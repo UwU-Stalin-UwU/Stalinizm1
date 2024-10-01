1af0501cfc78e9bd9918178488efda3bb13229c8
UwU-Stalin-UwU <JoBidon@tutamail.com>
2024-10-01 21:29:01 +0300

    Commit

--- IrkHolod.py
+++ IrkHolod.py
@@ -1,1 +1,11 @@
-print('Balkonskaya zakvashka')
+input_data = open('input.txt', 'r')
+a=input_data.readline()
+a=int(a)
+sum=0
+while a!=0 :
+    sum=sum+a
+    a=a-1
+output_data=open('output.txt', 'w')
+output_data.write(str(sum))
+input_data.close()
+output_data.close()
